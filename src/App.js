57dafd2f27eb9ff3a40fe056bee5b7a47b951b4a
Jerry Kao <46041407+jerry19980310@users.noreply.github.com>
2025-04-02 21:18:56 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   const { t } = useTranslation();
 
   return (
-    <BrowserRouter basename="/MyPortfolio">
+    <BrowserRouter>
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
