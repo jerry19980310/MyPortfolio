227b324ccf8fa975e261615a0321c73dd56ab56b
jerry19980310 <a0989718985@gmail.com>
2025-04-02 17:09:58 +1000

    update path

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
 
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/MyPortfolio">
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
